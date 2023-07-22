65cfbc390c7667a0dc9b061beb888bf49920bb32
Mehtab Butt <mehtabbutt48@gmail.com>
2023-07-22 22:19:45 +0500

    update: .rb file names changed to snake case

--- ruby/a_prefix_and_suffix_array.rb
+++ ruby/a_prefix_and_suffix_array.rb
@@ -1,14 +1,14 @@
-def is_palindrome p_and_s
+def is_palindrome(p_and_s)
   palindrome = {}
   p_and_s.each do |ps|
     palindrome.key?(ps.reverse) ? palindrome[ps.reverse] += 1 : palindrome[ps] = 1
   end
-  palindrome.keys.length == p_and_s.length/2.0 ? 'Yes' : 'No'
+  palindrome.keys.length == p_and_s.length / 2.0 ? 'Yes' : 'No'
 end
 
 t = gets.to_i
 p_and_s = []
-while t > 0 do
+while t > 0
   n = gets.to_i
   p_and_s = gets.split(' ')
   puts is_palindrome(p_and_s)
